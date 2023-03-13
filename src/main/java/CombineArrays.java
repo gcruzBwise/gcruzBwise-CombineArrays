c8e1ea50b593ca4dc592794390bea7077b14835e
gcruzBwise <gcruz28@wgu.edu>
2023-03-13 01:23:25 +0000

    Complete

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class CombineArrays {
     /**
@@ -8,7 +9,21 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int a1 = arr1.length;
+        int b1 = arr2.length;
 
-        return null;
+         // resultant array size
+        int c1 = a1 + b1;
+
+          // create the resultant array
+        int [] c = new int[c1];
+
+          // using the pre-defined function arraycopy
+        System.arraycopy(arr1, 0, c, 0, a1);
+        System.arraycopy(arr2, 0, c, a1, b1);
+  
+        // prints the resultant array
+        System.out.println(Arrays.toString(c));
+        return c;
     }
 }
